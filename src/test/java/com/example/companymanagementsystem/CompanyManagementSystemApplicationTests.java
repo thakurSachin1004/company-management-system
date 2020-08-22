88c5b9b05b15b829338f75025407ac7d1cee78e1
sachinthakurst <sachin.t@techsophy.com>
2020-08-22 11:14:53 +0530

    initial commit

---
+++ src/test/java/com/example/companymanagementsystem/CompanyManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
